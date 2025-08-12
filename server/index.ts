788ca20d89633bb4e560be3d9447e2b7d26b5643
s1upy <slupy888@gmail.com>
2025-08-13 00:55:53 +0300

    Исправлен запуск на Windows

--- server/index.ts
+++ server/index.ts
@@ -62,9 +62,7 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
+    port
   }, () => {
     log(`serving on port ${port}`);
   });
